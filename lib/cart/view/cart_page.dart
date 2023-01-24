28e7afe856a448b99ccc974fa1e8f46fda3dbd8c
Abdusamad <abdukarimovabdusamad1998@gmail.com>
2023-01-24 21:55:07 +0500

    Initial commit

---
+++ lib/cart/view/cart_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_shopping_cart/cart/cart.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Cart')),
+      body: ColoredBox(
+        color: Colors.yellow,
+        child: Column(
+          children: const [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(32),
+                child: CartList(),
+              ),
+            ),
+            Divider(height: 4, color: Colors.black),
+            CartTotal()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CartList extends StatelessWidget {
+  const CartList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final itemNameStyle = Theme.of(context).textTheme.headline6;
+
+    return BlocBuilder<CartBloc, CartState>(
+      builder: (context, state) {
+        if (state is CartLoading) {
+          return const CircularProgressIndicator();
+        }
+        if (state is CartLoaded) {
+          return ListView.separated(
+            itemCount: state.cart.items.length,
+            separatorBuilder: (_, __) => const SizedBox(height: 4),
+            itemBuilder: (context, index) {
+              final item = state.cart.items[index];
+              return Material(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: ListTile(
+                  leading: const Icon(Icons.done),
+                  title: Text(item.name, style: itemNameStyle),
+                  trailing: IconButton(
+                    onPressed: (){
+                      context.read<CartBloc>().add(CartItemRemoved(item));
+
+                    },
+                    icon: Icon(Icons.delete,color: Colors.red,),
+                  ),
+                  // onLongPress: () {
+                  // },
+                ),
+              );
+            },
+          );
+        }
+        return const Text('Something went wrong!');
+      },
+    );
+  }
+}
+
+class CartTotal extends StatelessWidget {
+  const CartTotal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final hugeStyle =
+        Theme.of(context).textTheme.headline1?.copyWith(fontSize: 48);
+
+    return SizedBox(
+      height: 200,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocBuilder<CartBloc, CartState>(
+              builder: (context, state) {
+                if (state is CartLoading) {
+                  return const CircularProgressIndicator();
+                }
+                if (state is CartLoaded) {
+                  return Text('\$${state.cart.totalPrice}', style: hugeStyle);
+                }
+                return const Text('Something went wrong!');
+              },
+            ),
+            const SizedBox(width: 24),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Buying not supported yet.')),
+                );
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+              child: const Text('BUY'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
